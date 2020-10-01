4a9ae78e16a47ebc0c2a2e756dd689b2a561dc2a
SaiDeeksha29 <saideeksha29@gmail.com>
2020-10-01 16:32:15 +0530

    Added:Tests for User Entry

---
+++ test/java/com/blz/training/UserEntry.java
@@ -0,0 +1,38 @@
+package com.blz.training;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.*;
+
+public class UserEntry {
+
+	@Test
+	public void HappyTest() {
+		UserRegistration ur = new UserRegistration();
+		boolean firstName = ur.checkFirstName("Deeksha");
+		assertEquals(true, firstName);
+		boolean lastName = ur.checkLastName("Sai");
+		assertEquals(true, lastName);
+		boolean email = ur.checkEmail("abc.xyz@bl.co.in");
+		assertEquals(true, email);
+		boolean mobile = ur.checkMobile("91 8888888888");
+		assertEquals(true, mobile);
+		boolean password = ur.checkPassword("jhsvYTu@1");
+		assertEquals(true, password);
+	}
+
+	@Test
+	public void SadTest() {
+		UserRegistration ur = new UserRegistration();
+		boolean firstName = ur.checkFirstName("De");
+		assertEquals(false, firstName);
+		boolean lastName = ur.checkLastName("Sa");
+		assertEquals(false, lastName);
+		boolean email = ur.checkEmail("ab@bl.co");
+		assertEquals(false, email);
+		boolean mobile = ur.checkMobile("918888888888");
+		assertEquals(false, mobile);
+		boolean password = ur.checkPassword("ju@1");
+		assertEquals(false, password);
+	}
+}
