23cc16ca74c7648c175e1e5448b4fcc41bc8c7f8
SaiDeeksha29 <saideeksha29@gmail.com>
2020-10-02 18:59:09 +0530

    Refactor:Invalid User Details Exception

--- test/java/com/blz/training/TestCustomException.java
+++ test/java/com/blz/training/TestCustomException.java
@@ -8,35 +8,35 @@ import org.junit.Test;
 public class TestCustomException {
 
 	@Test
-	public void givenInvalidFirstName() {
+	public void givenInvalidFirstName() throws InvalidUserDetailsException {
 		UserRegistration ur = new UserRegistration();
 		boolean firstName = ur.checkFirstName("De");
 		Assert.assertEquals(false, firstName);
 	}
 
 	@Test
-	public void givenInvalidLastName() {
+	public void givenInvalidLastName() throws InvalidUserDetailsException {
 		UserRegistration ur = new UserRegistration();
 		boolean lastName = ur.checkFirstName("De");
 		Assert.assertEquals(false, lastName);
 	}
 
 	@Test
-	public void givenInvalidEmail() {
+	public void givenInvalidEmail() throws InvalidUserDetailsException {
 		UserRegistration ur = new UserRegistration();
 		boolean email = ur.checkEmail("ab@bl.co");
 		Assert.assertEquals(false, email);
 	}
 
 	@Test
-	public void givenInvalidMobile() {
+	public void givenInvalidMobile() throws InvalidUserDetailsException {
 		UserRegistration ur = new UserRegistration();
 		boolean mobile = ur.checkMobile("918888888888");
 		Assert.assertEquals(false, mobile);
 	}
 
 	@Test
-	public void givenInvalidCheckEmail() {
+	public void givenInvalidCheckEmail() throws InvalidUserDetailsException {
 		UserRegistration ur = new UserRegistration();
 		boolean password = ur.checkPassword("ju@1");
 		Assert.assertEquals(false, password);
